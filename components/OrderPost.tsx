e907c01e175879023b1c8cad317c090e050a6cc2
Abdelhamid Boumegouas <aboumegouass@gmail.com>
2022-04-08 11:05:46 +0100

    Se

--- components/OrderPost.tsx
+++ components/OrderPost.tsx
@@ -3,15 +3,15 @@ import React, { ReactElement } from 'react'
 import PropTypes from "prop-types";
 import LastSeen from './LastSeen';
 import { useTranslation } from "react-i18next";
-
-function OrderPost({ id, time, type, title }: any): ReactElement {
+import { motion } from 'framer-motion'
+function OrderPost({ id, time, type, title, animateVariants, handleClick }: any): ReactElement {
     const { t } = useTranslation();
 
     function switchType() {
         switch (type) {
             case 0:
                 return (<span className="app-label primary">{t('processing_text')}</span>)
-                
+
             case 1:
                 return (<span className="app-label orange">{t('delivering_text')}</span>)
 
@@ -26,19 +26,17 @@ function OrderPost({ id, time, type, title }: any): ReactElement {
         }
     }
     return (
-        <Link href={`/orders/${id}`}>
-            <a className='post-item post-order'>
-                <p className="meta-text">
-                    <span className="material-icons material-icons-outlined">
-                        schedule
-                    </span>
-                    <LastSeen date={time} />
-                </p>
-                <h4 className="title d-flex">
-                    {title} <span className='me-auto'>{switchType()}</span>
-                </h4>
-            </a>
-        </Link>
+        <motion.a onClick={handleClick} initial="hidden" variants={animateVariants} layoutId={id} animate="visible" custom={id} className='post-item post-order'>
+            <p className="meta-text">
+                <span className="material-icons material-icons-outlined">
+                    schedule
+                </span>
+                <LastSeen date={time} />
+            </p>
+            <h4 className="title d-flex">
+                {title} <span className='me-auto'>{switchType()}</span>
+            </h4>
+        </motion.a>
     )
 }
 
@@ -48,4 +46,6 @@ OrderPost.propTypes = {
     time: PropTypes.any,
     type: PropTypes.any,
     title: PropTypes.any,
+    animateVariants: PropTypes.any,
+    handleClick: PropTypes.func,
 }
